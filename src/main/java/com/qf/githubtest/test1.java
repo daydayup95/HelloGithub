ac3286cbe9997f31a79abf0452f2d7319984815a
xm@qq.com <xm@qq.com>
2018-11-14 19:09:33 +0800

    第一次提交 github的helloworld完成

---
+++ src/main/java/com/qf/githubtest/test1.java
@@ -0,0 +1,11 @@
+package com.qf.githubtest;
+
+/**
+ * 第一个远程GitHub dome
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("第一个远程github 你好呀！");
+
+    }
+}
