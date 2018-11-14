fadb69a684e081c495d41fd6719d2eba9b30958a
xm@qq.com <xm@qq.com>
2018-11-14 19:30:25 +0800

    提交第一次

--- src/main/java/com/qf/githubtest/test1.java
+++ src/main/java/com/qf/githubtest/test1.java
@@ -6,6 +6,6 @@ package com.qf.githubtest;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("第一个远程github 你好呀！");
-
+        System.out.println("改了第一次");
     }
 }
